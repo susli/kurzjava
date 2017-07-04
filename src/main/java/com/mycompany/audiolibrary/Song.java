6cd2f4b47add29bebd5e01f74912216fa9567c79
User <User@10.0.2.15>
2017-07-04 12:01:37 +0200

    Fixed javadoc, updated conditions

--- src/main/java/com/mycompany/audiolibrary/Song.java
+++ src/main/java/com/mycompany/audiolibrary/Song.java
@@ -1,8 +1,7 @@
 package com.mycompany.audiolibrary;
 
 /**
- * Class Song make own object, which is supposed to represent
- * one song with atributes.
+ * Class Song make own object, which is supposed to represent one song with attributes.
  */
 public class Song {
 
@@ -14,30 +13,31 @@ public class Song {
 	private String genre;
 	private String length;
 
-    /**
-     * Construktor of song.
-     * @param name String variable which represents name of song. Not null.
-     * @param interpret String variable which represents song's interpret name.
-     * @param album String variable which represents name of album from which song came.
-     * @param songNumber Int variable means number of song in an album. Not null.
-     * @param year Int variable represents year of relase the song. Not null.
-     * @param genre String variable means gendre of song.
-     * @param length String variable for length of song.
-     */
+	/**
+	 * Constructor of song.
+	 * 
+	 * @param name String variable which represents name of song. Not null.
+	 * @param interpret String variable which represents song's interpret name.
+	 * @param album String variable which represents name of album from which song came.
+	 * @param songNumber Integer variable means number of song in an album. Not null.
+	 * @param year Integer variable represents year of  the song. Not null.
+	 * @param genre String variable means genders of song.
+	 * @param length String variable for length of song.
+	 */
 	public Song(String name, String interpret, String album, int songNumber, int year, String genre, String length) {
 		super();
-		this.name=name;
-		this.interpret = interpret==null?"NaN":interpret;
-		this.album = album==null?"NaN":album;
+		this.name = name;
+		this.interpret = interpret == null ? "NaN" : interpret;
+		this.album = album == null ? "NaN" : album;
 		this.songNumber = songNumber;
 		this.year = year;
-		this.genre = genre==null?"NaN":genre;
-		this.length = length==null?"NaN":length;
+		this.genre = genre == null ? "NaN" : genre;
+		this.length = length == null ? "NaN" : length;
 	}
 
 	public Song() {
 		super();
-		this.name="name";
+		this.name = "name";
 		this.interpret = "interpret";
 		this.album = "album";
 		this.songNumber = 0;
@@ -46,60 +46,67 @@ public class Song {
 		this.length = "length";
 	}
 
-    /**
-     * Metod to return name of song.
-     * @return name. Not null.
-     */
+	/**
+	 * Method to return name of song.
+	 * 
+	 * @return name. Not null.
+	 */
 	public String getName() {
 		return name;
 	}
 
-    /**
-     * Metod to return interpret of song.
-     * @return interpret. Not null.
-     */
+	/**
+	 * Method to return interpret of song.
+	 * 
+	 * @return interpret. Not null.
+	 */
 	public String getInterpret() {
 		return interpret;
 	}
 
-    /**
-     * Metod to return an album of the song.
-     * @return album. Not null.
-     */
+	/**
+	 * Method to return an album of the song.
+	 * 
+	 * @return album. Not null.
+	 */
 	public String getAlbum() {
 		return album;
 	}
 
-    /**
-     * Metod to return number of song in an album.
-     * @return sumber of song. Not null.
-     */
+	/**
+	 * Method to return number of song in an album.
+	 * 
+	 * @return number of song. Not null.
+	 */
 	public int getSongNumber() {
 		return songNumber;
 	}
 
-    /**
-     * Metod to return year of song.
-     * @return year. Not null.
-     */
+	/**
+	 * Method to return year of song.
+	 * 
+	 * @return year. Not null.
+	 */
 	public int getYear() {
 		return year;
 	}
-    /**
-     * Metod to return genre of song.
-     * @return genre. Not null.
-     */
+
+	/**
+	 * Method to return genre of song.
+	 * 
+	 * @return genre. Not null.
+	 */
 	public String getGenre() {
 		return genre;
 	}
 
-    /**
-     * Metod to return length of song.
-     * @return lenth. Not null.
-     */
+	/**
+	 * Method to return length of song.
+	 * 
+	 * @return length. Not null.
+	 */
 	public String getLength() {
 		return length;
 	}
-	
-	
+
 }

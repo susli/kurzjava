a21a02e704dd2246d0c0a47bcfb84c1a16b6c539
susli <jiri.kudlata@gmail.com>
2017-07-05 10:38:20 +0200

    uprava GUI

--- src/main/java/com/mycompany/audiolibrary/SceneController.java
+++ src/main/java/com/mycompany/audiolibrary/SceneController.java
@@ -40,9 +40,6 @@ public class SceneController implements Initializable {
 	@FXML // URL location of the FXML file that was given to the FXMLLoader
 	private URL location;
 
-	@FXML // fx:id="removeItem"
-	private MenuItem removeItem; // Value injected by FXMLLoader
-
 	@FXML // fx:id="searchButton"
 	private Button searchButton; // Value injected by FXMLLoader
 
@@ -90,7 +87,6 @@ public class SceneController implements Initializable {
        
 	@FXML // This method is called by the FXMLLoader when initialization is complete
 	void initialize() {
-		assert removeItem != null : "fx:id=\"removeItem\" was not injected: check your FXML file 'Scene.fxml'.";
 		assert searchButton != null : "fx:id=\"searchButton\" was not injected: check your FXML file 'Scene.fxml'.";
 		assert trackNumber != null : "fx:id=\"trackNumber\" was not injected: check your FXML file 'Scene.fxml'.";
 		assert year != null : "fx:id=\"year\" was not injected: check your FXML file 'Scene.fxml'.";

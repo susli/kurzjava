6b9c1ee23939c5bb4fc8f68a2148976260ea7471
OgarXVI <ogarxvi@gmail.com>
2017-07-05 11:12:17 +0200

    Long Song

--- src/main/java/com/mycompany/audiolibrary/SongDaoImpl.java
+++ src/main/java/com/mycompany/audiolibrary/SongDaoImpl.java
@@ -10,15 +10,18 @@ public class SongDaoImpl implements SongDao{
 	
 	public SongDaoImpl() {
 		songs = new ArrayList<>(); 
-		songs.add(new Song("IDK", "Corko", "Húko", 12, 1999, "speed metal", "12:12"));
-		songs.add(new Song("Netuším", "Lopata", "Ostrava", 12, 2015, "slow metal", "16:33"));
-		songs.add(new Song("IDK", "Nikoto", "Assembly", 12, 2014, "pop", "3:13"));
-		songs.add(new Song("Možná", "Heyja", "Húko", 6, 1999, "speed metal", "4:50"));
-		songs.add(new Song("Jako", "Despacito", "Lusi", 3, 2017, "pop", "4:51"));
-		songs.add(new Song("Jako", "Kura", "Makeš", 2, 2012, "rock", "5:40"));
-		songs.add(new Song("IDK", "Mezinkoš", "Makeš", 3, 2017, "rock", "4:13"));
-		songs.add(new Song("Neruš", "basket", "kRál", 1, 1999, "pop", "4:11"));
-		songs.add(new Song("Never", "Mezinkoš", "Makeš", 3, 2017, "rock", "4:13")); 
+		songs.add(new Song("Motor", "Corko", "Húko", 12, 1999, "speed metal", "12:12"));
+		songs.add(new Song("Netuší", "Lopata", "Ostrava", 12, 2015, "slow metal", "16:33"));
+		songs.add(new Song("Vyprchává", "Nikoto", "Assembly", 12, 2014, "pop", "3:13"));
+		songs.add(new Song("Maybe", "Heyja", "Húko", 6, 1999, "speed metal", "4:50"));
+		songs.add(new Song("Despacito", "Lusi", "WhAt", 3, 2017, "pop", "4:51"));
+		songs.add(new Song("Kdo to ví?", "Kura", "Makeš", 2, 2012, "rock", "5:40"));
+		songs.add(new Song("Posel", "Mezinkoš", "Makeš", 3, 2017, "rock", "4:13"));
+		songs.add(new Song("Neruš", "basket", "kRál", 1, 1999, "k-pop", "4:11"));
+		songs.add(new Song("Never", "Mezinkoš", "Makeš", 3, 2017, "rock", "3:13"));
+		songs.add(new Song("Koktejl", "Bunda", "Molez", 1, 2015, "rap", "6:24")); 
+		songs.add(new Song("Hruza", "aDěs", "Makeš", 3, 2017, "rock", "7:31"));
+		songs.add(new Song("Tento text má strašlivě dlouhý důvod ke své existenci, ale to je na jiné povídaní. Chcete ho slyšet? Tak tedy dobře... Slyšte slyšte - přichází Mistr Bard. Byl nebyl jeden svět, kde existoval mír, ale nebyl to jen takový mír... Exitovali toitž roboti. Ti se měli rádi - dokud byly v aréně. Mlátili se a ničili, až hrůza. Ale všichni byly spojeni a tak byl na světě mír. WIN-WIN.", "Mr.Robot", "Robots", 3, 2017, "robotic", "59:55"));
 	}
 	
 	@Override
